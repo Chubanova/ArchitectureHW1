bed1406b20027b6aca44511f716d2d3271df396d
Chubanova <m.esaeva@inbox.ru>
2023-07-03 13:19:53 +0300

    Empty project

---
+++ src/test/java/com/chubanova/DummyTest.java
@@ -0,0 +1,14 @@
+package com.chubanova;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void dummy() {
+        assertTrue(true);
+    }
+
+}
